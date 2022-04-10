461b42df4df3cc9ab431687bbd144563513f0abc
shambhavi <shambhavisinha077@gmail.com>
2022-04-10 16:18:57 +0530

    Calculator-App

---
+++ app/src/main/java/com/example/calculator/splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, 4000);
+    }
+}
